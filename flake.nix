41aacedfd6c12de63211b35baee39b40cb72a817
Mohan Ranganath Varma Vummadisetty <106021733+Raphael-08@users.noreply.github.com>
2024-10-31 23:34:17 +0530

    Update to 1.0.1-a.16

--- flake.nix
+++ flake.nix
@@ -8,15 +8,15 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.1-a.7";
+      version = "1.0.1-a.16";
       downloadUrl = {
         "specific" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-	  sha256 = "sha256:1dlb2cl86ndsl6b6jv7qr7rdg2rzqjgn3y8rlw6d8jj5r0giyinh";
+	  sha256 = "sha256:0nmcr4czycgabj5zdxa7sllbnhmcn90kh55f78n9x88cda481q9f";
 	};
 	"generic" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-	  sha256 = "sha256:17c1ayxjdn8c28c5xvj3f94zjyiiwn8fihm3nq440b9dhkg01qcz";
+	  sha256 = "sha256:1mkc2hpj2n0a9aj6g6yz4gx7j2lcdjhxhil5jhixbddpfp84yvcn";
 	};
       };
 
